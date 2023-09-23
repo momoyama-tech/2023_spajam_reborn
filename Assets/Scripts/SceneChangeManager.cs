6a8c709afcb4f394179510430ae674d4ed8c0323
deforu <133000379+deforu@users.noreply.github.com>
2023-09-24 03:06:38 +0900

    add mock

---
+++ Assets/Scripts/SceneChangeManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SwitcLobbyScene()
+    {
+        SceneManager.LoadScene("Lobby", LoadSceneMode.Single);
+    }
+    public void SwitchHistoryScene()
+    {
+        SceneManager.LoadScene("History", LoadSceneMode.Single);
+    }
+}
